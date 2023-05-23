ef332ba5fca00360736f0ee008703e00c4420580
yesman <80596641+yesman01@users.noreply.github.com>
2023-05-23 09:43:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 
 import random
 from time import sleep
-
-
-Begin1 = {"b","d","g","skr","sh","shl","chol"}
-Mid1   = {"onk","ing","ink","oink","oing","ong","ung","unk"}
-End11  = {"y","ly"}
-End21  = {"us","lus","aloo"}
 bongus = list({" is how its done", "is what we live for", " is whats going on", " is what we praise to", " overpowers everyone"})
-Begin = list(Begin1)
-Mid = list(Mid1)
-End1 = list(End11)
-End2 = list(End21)
-
+Begin = list({"b","d","g","skr","sh","shl","chol"})
+Mid = list({"onk","ing","ink","oink","oing","ong","ung","unk"})
+End1 = list({"y","ly"})
+End2 = list({"us","lus","aloo"})
 while True:
-    
     g = str(random.choice(Begin) + random.choice(Mid) + random.choice(End1) + " " + random.choice(Begin) + random.choice(Mid) + random.choice(End2) + random.choice(bongus))
     print(g)
     sleep(1)
